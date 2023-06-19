7acc557ff91465ef72fe0a2f4dabac2aab25b12a
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-19 18:04:49 +0300

    Update ZenX.lua

--- ZenX.lua
+++ ZenX.lua
@@ -1,7 +1,6 @@
 local whitelist = {
     Usernames = {
         "Wizard_CatWhite",
-        "TRUZER68",
         "EeMe6i",
         "CuteG0D",
     },
@@ -9,28 +8,25 @@ local whitelist = {
         00000000,
         11111111,
         22222222,
-
     }
-
 }
 
 local isPassed = false
 local player = game.Players.LocalPlayer
+
 for i,v in pairs(whitelist.Usernames) do
     if player.Name == v then
         isPassed = true
     end
 end
+
 for i,v in pairs(whitelist.UserIds) do
     if player.UserId == v then
-       isPassed = true
-
+        isPassed = true
     end
-
 end
 
 if isPassed == false then
-
     player:Kick("You are not whitelisted!")
     task.wait(3)
     game:Shutdown()
