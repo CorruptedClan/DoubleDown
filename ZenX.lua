a4dcf8777892d62fc6a5e6feede02c237a51f1c8
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-07-13 20:02:46 +0300

    Update ZenX.lua

--- ZenX.lua
+++ ZenX.lua
@@ -22,7 +22,7 @@ local whitelist = {
         "markitus028",
         "nikhilsinght",
         "username",
-        "username",
+        "its_s1lent",
         "SandwitchPieMan",
         "FlxshPxpcxrn",
         "DarkSheadow3",
