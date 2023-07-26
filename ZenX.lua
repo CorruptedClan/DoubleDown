bfa037f52c056b4c9181cf85c7b7c074ca244c40
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-07-26 20:30:42 +0300

    Update ZenX.lua

--- ZenX.lua
+++ ZenX.lua
@@ -31,7 +31,7 @@ local whitelist = {
         "ccf2008",
         "bugha3q",
         "turboIine",
-        "username",
+        "milangiezen",
         "lI92Il",
         "ERBLIN_EXE",
         "tofuu392",
